656ac188b3e2779930ea720dfbe1fee2d52ac8a0
Gabriel Carrizo <gabriel.carrizo@now.se>
2018-02-07 15:01:05 +0100

    added lab 1

---
+++ Lab1/Lab1.c
@@ -0,0 +1,39 @@
+/* sample hello world program  *
+ *  C Michael Hanke 2006-10-19 */
+
+#include <stdio.h>
+#include <string.h>
+#include <mpi.h>
+
+int main(int argc, char **argv)
+{
+  int rank, size, tag, rc, i;
+  MPI_Status status;
+  char message[20];
+
+  rc = MPI_Init(&argc, &argv);
+  rc = MPI_Comm_size(MPI_COMM_WORLD, &size);
+  rc = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  tag = 100;
+  if (rank == 0) {
+	  strcpy(message, "Hello, world");
+  	for (i = 1; i < size; i++)
+  	    rc = MPI_Send(message, 13, MPI_CHAR, i, tag, MPI_COMM_WORLD);
+  }
+  else{
+	  rc = MPI_Recv(message, 13, MPI_CHAR, 0, tag, MPI_COMM_WORLD, &status);
+
+    rc = MPI_Send(&rank, sizeof(rank) , MPI_INT, 0, tag, MPI_COMM_WORLD);
+  }
+  if(rank == 0){
+    int tmp;
+    for(i=1; i < size; i++){
+      rc = MPI_Recv(&tmp, sizeof(rank), MPI_INT, i, tag, MPI_COMM_WORLD, &status);
+      printf("%d\n",i);
+      printf("Recieved from node %d : %.13s\n", tmp, message);
+    }
+  }
+  rc = MPI_Finalize();
+  return 0;
+}
